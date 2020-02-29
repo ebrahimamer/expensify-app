368d7f7ff7026e4a08a46b67018ab2985fd9f34f
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2020-02-29 03:21:07 +0200

    Configuring webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
+  // const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
+    mode: isProduction ? 'production' : 'development',
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public', 'dist'),
@@ -18,27 +20,27 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        // use: CSSExtract.extract({
+        //   use: [
+        //     {
+        //       loader: 'css-loader',
+        //       options: {
+        //         sourceMap: true
+        //       }
+        //     },
+        //     {
+        //       loader: 'sass-loader',
+        //       options: {
+        //         sourceMap: true
+        //       }
+        //     }
+        //   ]
+        // })
       }]
     },
-    plugins: [
-      CSSExtract
-    ],
+    plugins: [new MiniCssExtractPlugin()],
+    // plugins: [ CSSExtract ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
